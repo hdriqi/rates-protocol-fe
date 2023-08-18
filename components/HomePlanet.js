e05da07fdbbd174f524e242b11ee3eb48bb8bb20
hdriqi <riqi.asdf@gmail.com>
2023-08-18 23:11:40 +0700

    update: HomePlanet style

--- components/HomePlanet.js
+++ components/HomePlanet.js
@@ -28,8 +28,6 @@ export default function HomePlanet() {
     getData()
   }, [])
 
-  console.log(planetList)
-
   return (
     <div className="max-w-6xl mx-auto p-4">
       <div className="flex items-center justify-between">
@@ -45,8 +43,10 @@ export default function HomePlanet() {
       <div className="flex flex-wrap -mx-4 mt-4">
         {planetList.map((planet) => {
           return (
-            <div className="w-1/2 md:w-1/2 lg:w-1/4 p-4 " key={planet.nft_id}>
-              <Image className="border-2 border-gray-800 w-full" alt={`planet #${planet.nft_id}`} src={`${process.env.NEXT_PUBLIC_API_URL}/render/${planet.nft_id}`} width="333" height="500" />
+            <div className="w-1/2 md:w-1/2 lg:w-1/4 p-4" key={planet.nft_id}>
+              <div className="border-2 border-gray-800 hover:border-gray-300 cursor-pointer transition-all duration-300">
+                <Image className="transition-opacity duration-300 hover:opacity-80 w-full" alt={`planet #${planet.nft_id}`} src={`${process.env.NEXT_PUBLIC_API_URL}/render/${planet.nft_id}`} width="333" height="500" />
+              </div>
             </div>
           )
         })}
